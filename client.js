17aaa1ce26209d8a6c00ab89348e8289389f03fa
Isaac Brist <isaacbrist@gmail.com>
2022-06-18 10:30:03 -0500

    added delete button and deleteClickHandler function, which allows the entire table row to be deleted when clicking on the button.

--- client.js
+++ client.js
@@ -2,21 +2,30 @@ $(document).ready(readyNow);
 function readyNow() {
   //submit click listener
   $('#submit').on('click', submitClickHandler);
+  $('#employee-table').on('click', '#delete', deleteClickHandler);
 }
 
 //function to collect the info and push to dom
 function submitClickHandler() {
-  let name = $('#first-name-in').val();
+  let firstName = $('#first-name-in').val();
+  let lastName = $('#last-name-in').val();
+  let idNumber = $('#id-number-in').val();
+  let jobTitle = $('#job-title-in').val();
+  let annualSalary = $('#annual-salary-in').val();
   $('#employee-table').append(`<tr>
-  <td></td>
-  <td>${name}</td>
-  <td>Hello</td>
-  <td>Hello</td>
-  <td>Hello</td>
-  <td>Hello</td>`);
-  console.log();
+  <td class ='table-row'>${firstName}</td>
+  <td class ='table-row'>${lastName}</td>
+  <td class ='table-row'>${idNumber}</td>
+  <td class ='table-row'>${jobTitle}</td>
+  <td class ='table-row'>${annualSalary}</td>
+  <td class ='table-row'> <button id="delete">Delete</button</td>`);
+  $('input').val('');
 }
 
+function deleteClickHandler() {
+  $(this).parent().parent().remove();
+  console.log('deleted');
+}
 //The application should have an input form that collects:
 // employee:
 // first name,
